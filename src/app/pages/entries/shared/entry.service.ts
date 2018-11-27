67f4f1768ce4dea3c886fec2a2da53706dc031e5
JoelSchecheleski <joel.jsp@gmail.com>
2018-11-26 22:39:57 -0200

    Adding Entry service inproving

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -3,7 +3,7 @@ import {BaseResourceService} from '../../../shared/services/base-resource.servic
 import {Entry} from './entry.module';
 
 import {CategoryService} from '../../categories/shared/category.service';
-import {flatMap} from 'rxjs/operators';
+import {catchError, flatMap} from 'rxjs/operators';
 
 import {Observable} from 'rxjs';
 
@@ -19,21 +19,21 @@ export class EntryService extends BaseResourceService<Entry> {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-        return super.create(entry);
-      })
-    );
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this) );
   }
 
   update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this) );
+  }
+
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
         entry.category = category;
-        return super.update(entry);
-      })
+        return sendFn(entry);
+      }),
+      catchError(this.handleError)
     );
   }
-
 }
